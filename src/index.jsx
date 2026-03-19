0b64a8c9852740b1a2173ca4a44498f1e30dffae
Kyle Hammond <kyleroberthammond@gmail.com>
2026-03-19 01:07:03 +0000

    Converting to shared module

---
+++ src/index.jsx
@@ -0,0 +1,1 @@
+export { default as TableTorch } from './TableTorch'
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.jsx'),
+      name: 'TableTorch',
+      formats: ['es', 'cjs'],
+      fileName: (format) => format === 'es' ? 'tabletorch.js' : 'tabletorch.cjs'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  }
 })
